a252142c17b38a705dbde4a2f85771466b071302
Alex Mitzman and Evan Gryska <pair+alex+evan@allstate.com>
2016-06-20 17:01:23 -0500

    functioning tip calc, no working style

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, function(){
   console.log('server is listening');
 });
+
+app.get('/tip_calculator', function(req, res){
+  res.render('tip_calculator');
+})
+
+app.post('/tip_calculator', function(req, res){
+  const amount = req.body.amount;
+  const tipPercent = req.body.tipPercent;
+  const tip = amount*1 * tipPercent*1;
+  res.render('tip_calculator', {amount, tipPercent, tip})
+})
